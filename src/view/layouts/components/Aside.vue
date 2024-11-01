031cdc81bc9667ce4e82699c9e0895d39371d03c
Truong Pham Xuan <xuantruonglk02@gmail.com>
2024-11-02 00:33:43 +0700

    aside

--- src/view/layouts/components/Aside.vue
+++ src/view/layouts/components/Aside.vue
@@ -1,78 +1,62 @@
 <script setup lang="ts">
 import { House } from '@element-plus/icons-vue';
+import { PageName } from 'src/router/routes';
 import { useAppStore } from 'src/stores/appStore.ts';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const appStore = useAppStore();
+
+const aside = [
+  {
+    name: 'Home',
+    icon: House,
+    to: PageName.DASHBOARD_PAGE,
+  },
+  {
+    name: 'Dashboard',
+    icon: House,
+    to: PageName.DASHBOARD_PAGE,
+  },
+  {
+    name: 'Table',
+    icon: House,
+    to: PageName.TABLE_PAGE,
+  },
+  {
+    name: 'Card',
+    icon: House,
+    to: PageName.CARDS_PAGE,
+  },
+];
 </script>
 
 <template>
   <div :style="`height: calc(100vh - ${appStore.headerHeight}px); width: ${appStore.asideWidth}px`">
     <el-aside class="aside-ctn position-fixed h-100 z-1" :width="`${appStore.asideWidth}px`">
       <el-scrollbar>
-        <el-menu :default-openeds="['1', '3']">
-          <el-menu-item index="1">
-            <template #title>
-              <el-icon><House /></el-icon>
-              <span v-if="!appStore.asideCollapsed">Home</span>
-            </template>
+        <el-menu :collapse="appStore.asideCollapsed">
+          <el-menu-item
+            v-for="(item, index) in aside"
+            :key="index"
+            :index="index"
+            @click="router.push({ name: item.to })"
+          >
+            <!-- <el-icon>item.icon</el-icon> -->
+            <el-icon><House /></el-icon>
+            <template #title>{{ item.name }}</template>
           </el-menu-item>
-          <el-menu-item index="2">
-            <template #title>
-              <el-icon><House /></el-icon>
-              <span v-if="!appStore.asideCollapsed">Dashboard</span>
-            </template>
+          <el-divider />
+          <el-menu-item index="index" @click="router.push({ name: PageName.DASHBOARD_PAGE })">
+            <!-- <el-icon>item.icon</el-icon> -->
+            <el-icon><House /></el-icon>
+            <template #title>About</template>
           </el-menu-item>
-          <!-- <el-sub-menu index="1">
-              <template #title>
-                <el-icon><House /></el-icon>Navigator One
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="1-1">Option 1</el-menu-item>
-                <el-menu-item index="1-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="1-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>Option4</template>
-                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="2">
-              <template #title>
-                <el-icon><icon-menu /></el-icon>Navigator Two
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="2-1">Option 1</el-menu-item>
-                <el-menu-item index="2-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="2-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="2-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="3">
-              <template #title>
-                <el-icon><setting /></el-icon>Navigator Three
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="3-1">Option 1</el-menu-item>
-                <el-menu-item index="3-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="3-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="3-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu> -->
+          <el-menu-item index="index" @click="router.push({ name: PageName.DASHBOARD_PAGE })">
+            <!-- <el-icon>item.icon</el-icon> -->
+            <el-icon><House /></el-icon>
+            <template #title>App Gallery</template>
+          </el-menu-item>
         </el-menu>
       </el-scrollbar>
     </el-aside>
